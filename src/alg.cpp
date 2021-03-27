fb105e2047427f8b1795b5f5ff80fa1722ab86db
IvanTemp <78906461+IvanTemp@users.noreply.github.com>
2021-03-27 15:19:47 +0300

    test2

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,37 +1,5 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-      int res = 0, mid = 0, left = 0, right = size;
-    while (true)
-    {
-        mid = (right + left) / 2;
-        //cout << "left = " << left << "; right = " << right << "; mid = " << mid << endl;
-        if (value < arr[mid])
-        {
-            right = mid - 1;
-        }
-        else if (value > arr[mid])
-        {
-            left = mid + 1;
-        }
-        else
-        {
-            while (arr[mid] == value)
-            {
-                mid--;
-            };
-            mid++;
-            while (arr[mid] == value)
-            {
-                res++;
-                mid++;
-            };
-            break;
-        }
-        if (left > right)
-        {
-            break;
-        }
-    }
-    return res;
+      return 0;
 }
